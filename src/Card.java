ac1815a61d2fe826638ae9266f49684786adb215
phrezende-eng <ph.hr.001@gmail.com>
2024-06-30 18:02:34 -0300

    feat: add Card model

---
+++ src/Card.java
@@ -0,0 +1,11 @@
+class Card {
+    String number;
+
+    Card(String number) {
+        this.number = number;
+    }
+
+    boolean isFromStoreCard() {
+        // return number.startsWith("4296 13");
+    }
+}
