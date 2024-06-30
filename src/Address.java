570ebe6103d919171a7c47ad8445f70343a25e17
phrezende-eng <ph.hr.001@gmail.com>
2024-06-30 17:54:27 -0300

    feat: address model

---
+++ src/Address.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+class Address {
+    String state;
+    String city;
+
+    Address(String city, String state) {
+        this.city = city;
+        this.state = state;
+    }
+
+    @Test
+    String getRegion() {
+        // TODO:...
+    }
+
+    @Test
+    public boolean isCapital() {
+        // TODO:...
+    }
+}
