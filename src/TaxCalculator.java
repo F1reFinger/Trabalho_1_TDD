8f604866aec243064df5fd694eeb15b08daac91e
F1reFinger <helderbonzinho@gmail.com>
2024-07-31 21:38:36 -0300

    testing tax calculator

---
+++ src/TaxCalculator.java
@@ -0,0 +1,21 @@
+
+public class TaxCalculator {
+    public static final double DF_ICMS_TAX_RATE = 0.18;
+    public static final double ICMS_TAX_RATE = 0.12;
+    public static final double MUNICIPAL_TAX_RATE = 0.04;
+
+    public double calculateICMSTax(double price, String state) {
+        if (state.equals("DF")) {
+            return price * DF_ICMS_TAX_RATE;
+        } else {
+            return price * ICMS_TAX_RATE;
+        }
+    }
+
+    public double calculateMunicipalTax(double price, String state) {
+        if (!state.equals("DF")) {
+            return price * MUNICIPAL_TAX_RATE;
+        }
+        return 0.0;
+    }
+}
